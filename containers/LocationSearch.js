920db922cf4cd996000b1f69e235eaa0f089d7a3
ranyehushua <ranyehushua@gmail.com>
2017-03-08 13:01:26 -0700

    Updated Location Search to fetch from server API

--- containers/LocationSearch.js
+++ containers/LocationSearch.js
@@ -9,21 +9,41 @@ import Search from '../components/Search';
 export default class LocationSearch extends Component {
   constructor(props) {
     super(props);
-    this.state = {input: ''};
+    this.state = {
+      input: '',
+      response: null
+    };
   }
 
 
   onTextSubmit = (text) => {
     this.setState({input: text});
     //Take state.input and pass it to the API call here
+    fetch('http://127.0.0.1:3030/')
+      .then((data) => data.json())
+      .then((data) => {
+        this.setState({response: data});
+        console.log(this.state.response);
+      }).catch(error => console.error(error));
   }
 
   render() {
     return (
       <View>
         <Search onSubmit={this.onTextSubmit} />
-        {this.state.input.length > 0
-          ? <Text>{this.state.input}</Text>
+        {this.state.response
+          ? <View>
+            <Text>Location: {this.state.response.city}, {this.state.response.state}</Text>
+            {this.state.response.actionDay
+              ? <Text>ACTION DAY!</Text>
+              : null
+            }
+            <Text>Date/Time: {this.state.response.dateTime}</Text>
+            <Text>Ozone AQI: {this.state.response.ozoneAQI}</Text>
+            <Text>Ozone Category: {this.state.response.ozoneAQIcategory}</Text>
+            <Text>PM 2.5 AQI: {this.state.response.pm25AQI}</Text>
+            <Text>PM 2.5 Category: {this.state.response.pm25AQIcategory}</Text>
+          </View>
           : null
         }
       </View>
